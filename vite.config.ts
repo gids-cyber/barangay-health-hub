3406915e9ab1b21175d5aa446ae305a4cc77ecb7
gids <gabsgids2021@gmail.com>
2026-04-01 00:04:18 +0800

    Commmit

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/barangay-health-hub',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
